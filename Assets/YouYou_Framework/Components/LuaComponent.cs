ef6297658824052f3bc1066f9d51cd36fe16b580
wangyibo <89531734@qq.com>
2019-09-25 00:58:54 +0800

    9.25

--- Assets/YouYou_Framework/Components/LuaComponent.cs
+++ Assets/YouYou_Framework/Components/LuaComponent.cs
@@ -12,22 +12,66 @@ namespace YouYou
     {
         private LuaManager m_LuaManager;
 
+        /// <summary>
+        /// 是否打印协议日志
+        /// </summary>
+        public bool DebugLogProto = false;
+
         protected override void OnAwake()
         {
             base.OnAwake();
-
             m_LuaManager = new LuaManager();
+
+#if DEBUG_LOG_PROTO
+            DebugLogProto=true;
+#endif
         }
 
         protected override void OnStart()
         {
             base.OnStart();
+            LoadDataTableMS = new MMO_MemoryStream();
             m_LuaManager.Init();
+        }
+
+        /// <summary>
+        /// 加载数据表的MS
+        /// </summary>
+        public MMO_MemoryStream LoadDataTableMS
+        {
+            get;
+            private set;
+        }
+
+        public MMO_MemoryStream LoadDataTable(string tableName)
+        {
+            byte[] buffer = GameEntry.Resource.GetFileBuffer(string.Format("{0}/download/DataTable/{1}.bytes", GameEntry.Resource.LocalFilePath, tableName));
+
+            LoadDataTableMS.SetLength(0);
+            LoadDataTableMS.Write(buffer, 0, buffer.Length);
+            LoadDataTableMS.Position = 0;
+
+            return LoadDataTableMS;        
+        }
+
+        /// <summary>
+        /// 在Lua中加载MemoryStream
+        /// </summary>
+        /// <param name="buffer"></param>
+        /// <returns></returns>
+        public MMO_MemoryStream LoadSocketReceiveMS(byte[] buffer)
+        {
+            MMO_MemoryStream ms = GameEntry.Socket.SocketReceiveMS;
+            ms.SetLength(0);
+            ms.Write(buffer, 0, buffer.Length);
+            ms.Position = 0;
+            return ms;
         }
 
         public override void Shutdown()
         {
-            
+            LoadDataTableMS.Dispose();
+            LoadDataTableMS.Close();
         }
     }
 }
--- Assets/YouYou_Framework/Managers/Lua/LuaForm.cs
+++ Assets/YouYou_Framework/Managers/Lua/LuaForm.cs
@@ -2,10 +2,30 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using XLua;
 
 namespace YouYou
 {
+    /// <summary>
+    /// Lua组件类型
+    /// </summary>
+    public enum LuaComType
+    {
+        GameObject=0,
+        Transform=1,
+        Button=2,
+        Image=3,
+        YouYouImage=4,
+        Text=5,
+        YouYouText=6,
+        RawImage=7,
+        InputField=8,
+        Scrollbar=9,
+        ScrollView=10,
+        MulityScroller=11
+    }
+
     /// <summary>
     /// Lua窗口
     /// </summary>
@@ -30,6 +50,49 @@ namespace YouYou
 
         private LuaTable scriptEnv;
         private LuaEnv luaEnv;
+
+        [Header("Lua组件")]
+        [SerializeField]
+        private LuaCom[] m_LuaComs;
+
+        /// <summary>
+        /// 根据索引来查找组件
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public object GetLuaComs(int index)
+        {
+            LuaCom com = m_LuaComs[index];
+
+            switch (com.Type)
+            {
+                case LuaComType.GameObject:
+                    return com.Trans.gameObject;
+                case LuaComType.Transform:
+                    return com.Trans.gameObject;
+                case LuaComType.Button:
+                    return com.Trans.GetComponent<Button>();
+                case LuaComType.Image:
+                    return com.Trans.GetComponent<Image>();
+                case LuaComType.YouYouImage:
+                    return com.Trans.GetComponent<YouYouImage>();
+                case LuaComType.Text:
+                    return com.Trans.GetComponent<Text>();
+                case LuaComType.YouYouText:
+                    return com.Trans.GetComponent<YouYouText>();
+                case LuaComType.RawImage:
+                    return com.Trans.GetComponent<RawImage>();
+                case LuaComType.InputField:
+                    return com.Trans.GetComponent<InputField>();
+                case LuaComType.Scrollbar:
+                    return com.Trans.GetComponent<Scrollbar>();
+                case LuaComType.ScrollView:
+                    return com.Trans.GetComponent<ScrollRect>();
+                case LuaComType.MulityScroller:
+                    return com.Trans.GetComponent<UIMultiScroller>();
+            }
+            return com.Trans;
+        }
 
         protected override void OnInit(object userData)
         {
@@ -93,6 +156,28 @@ namespace YouYou
             onClose = null;
             onBeforDestroy = null;
         }
+    }
+
+    [Serializable]
+    /// <summary>
+    /// Lua组件
+    /// </summary>
+    public class LuaCom
+    {
+        /// <summary>
+        /// 名称
+        /// </summary>
+        public string Name;
+
+        /// <summary>
+        /// 类型
+        /// </summary>
+        public LuaComType Type;
+
+        /// <summary>
+        /// 变换
+        /// </summary>
+        public Transform Trans;
     }
 }
 
