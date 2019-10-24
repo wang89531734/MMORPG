2ed7bce541816992c509592558900dd64c8eb179
wangyibo <89531734@qq.com>
2019-10-24 23:07:47 +0800

    106事件

--- Assets/YouYou_Framework/Managers/Event/CommonEventId.cs
+++ Assets/YouYou_Framework/Managers/Event/CommonEventId.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CommonEventId : MonoBehaviour
+public class CommonEventId 
 {
-
+    public const ushort Complete = 10001;
 }
